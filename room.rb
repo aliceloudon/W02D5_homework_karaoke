ff79a5a665ca97f73f869e6f13027a47161505b2
Alice Loudon <alice_loudon@icloud.com>
2017-02-10 15:15:44 +0000

    Pass test_show_capacity for room.rb

---
+++ room.rb
@@ -0,0 +1,11 @@
+class Room
+
+  def initialize(capacity)
+    @capacity = capacity
+  end
+
+  def show_capacity
+    return @capacity
+  end
+
+end
--- specs/room_spec.rb
+++ specs/room_spec.rb
@@ -1,13 +1,18 @@
 require('minitest/autorun')
 require('minitest/rg')
 require_relative('../room.rb')
+require_relative('../guest.rb')
+require_relative('../song.rb')
 
 class TestRoom < MiniTest::Test
 
   def setup
-    @room1 = Room.new()    
+    @room1 = Room.new(10)    
+  end
+
+  def test_show_capacity
+    assert_equal(10, @room1.show_capacity)
   end
 
 end
-
 
