0e313d4d659533962b0f4b09fb6bcd794b07d8d9
Alice Loudon <alice_loudon@icloud.com>
2017-02-12 16:40:39 +0000

    Runner includes input for room, guest and song

--- runner.rb
+++ runner.rb
@@ -1,7 +1,3 @@
-require_relative ('room')
-require_relative ('guest')
-require_relative ('song')
-
 # For use by the staff at CCC:
 # Type in the name of the room.
 # Check-in guest(s) to the room. 
@@ -9,54 +5,76 @@ require_relative ('song')
 # Check-out guest(s) from the room.
 # Confirm if guests have enough money to pay fee.
 
+require_relative ('room')
+require_relative ('guest')
+require_relative ('song')
+
 puts "Welcome to CodeClanCaraoke!"
-puts "Let's make a new room. What would you like to call it?"
-room_name = gets.chomp
-puts
-puts "How many people will be able to use the room?"
-room_capacity = gets.chomp
-puts
-puts "What will the entry fee be?"
-entry_fee = gets.chomp
-puts
-puts "Great, you have set up a new room called #{room_name}, which has a capacity of #{room_capacity} and an entry fee of #{entry_fee}."
+puts "What would you like to do first? Add a room, guest or song?"
+answer = gets.chomp
 
-room1 = Room.new(room_name, room_capacity, [], [], entry_fee)
+if answer == "room"
 
-puts "Now, let's add some guests to #{room_name}. Enter the name of the first guest"
-guest1 = gets.chomp
+  puts
+  puts "Great. What would you like to call the room?"
+  room_name = gets.chomp
 
-room1.add_guest(guest1)
+  puts
+  puts "How many people will be able to use the room?"
+  room_capacity = gets.chomp
 
-puts
-puts "How much money do they have on them?"
-money1 = gets.chomp
+  puts
+  puts "What will the entry fee be?"
+  entry_fee = gets.chomp
 
-if money1 < entry_fee
-  print "Oh no! #{guest1} doesn't have enough money to use #{room_name}."
-else
-  print nil
+  puts
+  puts "Great, you have set up a new room called #{room_name}, which has a capacity of #{room_capacity} and an entry fee of #{entry_fee}."
+
+  # @room1 = Room.new(room_name, room_capacity, [], [], entry_fee)
+  # puts @room1.show_entry_fee
+
+elsif answer == "guest"
+
+  puts
+  puts "What is the name of the guest?"
+  guest_name = gets.chomp
+
+  puts
+  puts "How much money does #{guest_name} have (in GBP)?"
+  guest_money = gets.chomp
+
+  # if money >= entry_fee
+  #   puts nil
+  # else
+  #   puts "Oh no! #{guest} doesn't have enough money to use #{room_name}."
+  # end
+
+  puts
+  puts "What is their favourite song?"
+  favourite_song = gets.chomp
+
+  puts
+  puts "Great, you have added a new guest called #{guest_name}, who has #{guest_money} and their favourite song is #{favourite_song}."
+
+  # @guest1 = Guest.new(guest_name, guest_money, favourite_song)
+
+
+elsif answer == "song"
+
+  puts
+  puts "What is the name of the song you would like to add to the playlist?"
+  song_name = gets.chomp
+
+  puts
+  puts "What is the length of the song (in minutes)?"
+  song_length = gets.chomp
+
+  puts
+  puts "OK. You have added a new song called #{song_name}, with a length of #{song_length}."
+
+else 
+
+  puts "Sorry, I didn't quite get that. Please type in 'room', 'guest' or 'song'."
+
 end
-
-# puts
-# puts "What is their favourite song?"
-# favourite_song1 = gets.chomp
-# puts
-# puts "Great, you have added a new guest called #{guest1}, who has #{money1} and their favourite song is #{favourite_song1}"
-
-# guest1 = Guest.new(guest1, money1, favourite_song1)
-
-# puts
-# puts "Now enter the name of the second guest"
-# guest2 = gets.chomp
-# puts
-# puts "How much money do they have on them?"
-# money2 = gets.chomp
-# puts
-# puts "And do they have a favourite song?"
-# favourite_song2 = gets.chomp
-# puts
-# puts "Great, you have added a new guest called #{guest2}, who has #{money2} and their favourite song is #{favourite_song2}"
-
-# guest2 = Guest.new(guest2, money2, favourite_song2)
 
